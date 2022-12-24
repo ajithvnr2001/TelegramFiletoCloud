23763a861d1b6f4b4185daea4f72704e89a35da1
ajithvnr2001 <74783525+ajithvnr2001@users.noreply.github.com>
2022-12-24 18:39:40 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,6 +1,6 @@
 class config():
-    BOT_TOKEN = ''  # from @botfather
-    APP_ID = ''     # from https://my.telegram.org/apps
-    API_HASH = ''   # from https://my.telegram.org/apps
-    API_KEY = ''    # from https://mixdrop.co
-    API_EMAIL = ''  # from https://mixdrop.co
+    BOT_TOKEN = '5984827825:AAHkIyha8B0rgP87nMYGmepDbvIi6NJKyXA'  # from @botfather
+    APP_ID = '1954515'     # from https://my.telegram.org/apps
+    API_HASH = '9ff4a51876a555bdb530cd6a58baae87'   # from https://my.telegram.org/apps
+    API_KEY = 'YIVdUQrV4N9EPHiw'    # from https://mixdrop.co
+    API_EMAIL = 'multdropdec24@triots.com'  # from https://mixdrop.co
